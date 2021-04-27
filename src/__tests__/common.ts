1c616e66c3f892c0b63954f0e49c335683aea2da
Karl von Randow <karl@xk72.com>
2021-04-28 10:59:19 +1200

    testing: use npm install to test
    As we cannot do a test compile using typescript without installing the dependencies

--- src/__tests__/common.ts
+++ src/__tests__/common.ts
@@ -1,41 +1,34 @@
-import ts from 'typescript'
-import glob from 'glob-promise'
+import { exec } from 'child_process'
 import path from 'path'
 import { CodegenConfig } from '@openapi-generator-plus/types'
 import { CodegenResult, createCodegenResult } from '@openapi-generator-plus/testing'
 import createGenerator from '..'
 
 export const DEFAULT_CONFIG: CodegenConfig = {
-	maven: {},
+	npm: {
+		name: 'test',
+	},
 }
 
 export async function prepare(spec: string, config?: CodegenConfig): Promise<CodegenResult> {
 	return createCodegenResult(path.resolve(__dirname, spec), config || DEFAULT_CONFIG, createGenerator)
 }
 
-export async function compile(basePath: string): Promise<void> {
-	const searchPath = path.join(basePath, '**/*.ts')
-	const paths = await glob(searchPath)
-	if (!paths.length) {
-		throw new Error('No files found to compile')
-	}
-	const program = ts.createProgram(paths, {
-		noEmitOnError: true,
-		noImplicitAny: true,
-		target: ts.ScriptTarget.ES2019,
-		module: ts.ModuleKind.CommonJS,
-		skipDefaultLibCheck: true,
-		skipLibCheck: true,
-		strict: true,
+export async function build(basePath: string): Promise<void> {
+	return new Promise(function(resolve, reject) {
+		exec(
+			'npm install 2>&1',
+			{
+				cwd: basePath,
+			},
+			function(error, stdout, stderr) {
+				if (!error) {
+					resolve()
+					return
+				}
+				reject(`Failed to build ${basePath}:\n${stdout}`)
+			}
+		)
 	})
-	const result = program.emit()
-	if (result.emitSkipped && result.diagnostics.length) {
-		const diagnostics = result.diagnostics
-			.map(d => `${d.file ? d.file.fileName : ''}:${d.file && d.start !== undefined ? d.file.getLineAndCharacterOfPosition(d.start).line : '?'} ${d.messageText}`)
-		throw new Error(`Failed to compile. ${diagnostics.length} diagnostics:\n  ${diagnostics.join('\n  ')}`)
-	} else if (result.diagnostics.length) {
-		for (const d of result.diagnostics) {
-			console.warn(d.messageText)
-		}
-	}
+	
 }
