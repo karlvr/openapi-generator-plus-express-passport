bbbc6aabe03ce105af1ec568284d0bf2ce937a71
Karl von Randow <karl@xk72.com>
2021-08-19 13:42:48 +1200

    testing: update test timeout configuration

--- src/__tests__/compile.spec.ts
+++ src/__tests__/compile.spec.ts
@@ -3,19 +3,21 @@ import { build, prepare, DEFAULT_CONFIG } from './common'
 import fs from 'fs'
 import path from 'path'
 
-const basePath = path.join(__dirname, '..', '..', '__tests__', 'specs')
-const files = fs.readdirSync(basePath)
+describe('compile test cases', () => {
+	const basePath = path.join(__dirname, '..', '..', '__tests__', 'specs')
+	const files = fs.readdirSync(basePath)
 
-for (const file of files) {
-	test(file, async() => {
-		const result = await prepare(path.join(basePath, file), {
-			...DEFAULT_CONFIG,
-			npm: {
-				...(DEFAULT_CONFIG.npm || {}),
-				name: path.basename(file),
-			},
-			includeTests: true,
+	for (const file of files) {
+		test(file, async() => {
+			const result = await prepare(path.join(basePath, file), {
+				...DEFAULT_CONFIG,
+				npm: {
+					...(DEFAULT_CONFIG.npm || {}),
+					name: path.basename(file),
+				},
+				includeTests: true,
+			})
+			await testGenerate(result, build, path.join('test-output', file))
 		})
-		await testGenerate(result, build, path.join('test-output', file))
-	}, 20000)
-}
+	}
+})
